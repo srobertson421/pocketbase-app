9c18ac383e3438080152d621b43d5fe09815e73b
Sean Robertson <srobertson421@gmail.com>
2022-11-23 13:05:49 -0800

    added admin to dashboard. Added todos collection and first todo. Added logic to render todos

--- pb_public/app.js
+++ pb_public/app.js
@@ -1,6 +1,8 @@
 const countEl = document.getElementById('count');
 const incBtn = document.getElementById('inc');
 const decBtn = document.getElementById('dec');
+const todosFetchBtn = document.getElementById('todosFetchBtn');
+const todosEl = document.getElementById('todos');
 
 incBtn.addEventListener('click', () => {
   const currentCount = parseInt(countEl.innerText.split(' ')[1]);
@@ -12,4 +14,17 @@ decBtn.addEventListener('click', () => {
   const currentCount = parseInt(countEl.innerText.split(' ')[1]);
 
   countEl.innerText = `Count: ${currentCount - 1}`;
+});
+
+todosFetchBtn.addEventListener('click', () => {
+  fetch('/api/collections/todos/records')
+  .then(res => res.json())
+  .then(data => {
+    const todos = data.items;
+
+    todos.forEach(todo => {
+      todosEl.insertAdjacentHTML('beforeend', `<h4>${todo.text}</h4>`);
+    });
+  })
+  .catch(console.log);
 });
